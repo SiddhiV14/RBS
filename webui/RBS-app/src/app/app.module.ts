b47424b3b7c97bdbe9f80df8487d20d445f08453
RuchiS-hexaware <RuchiS@hexaware.com>
2020-12-25 20:31:26 +0530

    view rates component added

--- webui/RBS-app/src/app/app.module.ts
+++ webui/RBS-app/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewRatesComponent } from './view-rates/view-rates.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{path:"rate", component:ViewRatesComponent}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewRatesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
