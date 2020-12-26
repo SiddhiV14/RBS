df2e64393a4af94e5968e50e3c3c969b324dde01
SiddhiV14 <siddhivyawahare99@gmail.com>
2020-12-26 22:34:45 +0530

    home page :wq

--- webui/RBS-app/src/app/app.module.ts
+++ webui/RBS-app/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewRateComponent } from './view-rate/view-rate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewRateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot([
+      {path:"",redirectTo:"home",pathMatch:"full"},
+      {path:"home",component:HomeComponent},
+      {path:"view-rate",component:ViewRateComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
