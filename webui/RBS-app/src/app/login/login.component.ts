408fc1623b0ffe7a51d5811f2d6fa22af57c0ec5
SiddhiV14 <siddhivyawahare99@gmail.com>
2021-01-02 23:03:54 +0530

    login

--- webui/RBS-app/src/app/login/login.component.ts
+++ webui/RBS-app/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +11,23 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   f:FormGroup;
 
-  constructor(private route :Router) { }
+  constructor(private route :Router,private login:LoginService) { }
 
   ngOnInit(): void {
     this.f=new FormGroup({
       uname:new FormControl(''),
-      pswd:new FormControl('')
+      pswd:new FormControl(''),
+      
     })
   }
   onSubmit(f:any) {
     console.log(f.uname);
     console.log(f.pswd);
+    this.login.createUser(f.uname).subscribe((response)=>{
+      console.log("user has been added")
+     
+    })
+  
     this.route.navigate(["home"]);
   }
 }
